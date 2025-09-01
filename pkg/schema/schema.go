3ceb83b08e50224fd3590f7e8d5e40ecedd41c80
Steve Moyer <smoyer1@selesy.com>
2025-09-01 10:09:32 -0400

    feat(schema): add the generated, "raw" JSONSchemas for Ethereum methods

---
+++ pkg/schema/schema.go
@@ -0,0 +1,13 @@
+//go:generate go run ../../gen/schema/
+
+// Package schema contains the "raw" JSONSchema source for each OpenRPC
+// method defined by Ethereum's execution APIs.
+package schema
+
+// Schema returns the JSONSchema source for the Ethereum method with the
+// matching name.
+func Schema(name string) (string, bool) {
+	schema, ok := Schemas()[name]
+
+	return schema, ok
+}
