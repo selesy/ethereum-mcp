4e2cd4d0eb86e04156d947c49e8ed6c448457d3e
Steve Moyer <smoyer1@selesy.com>
2025-09-10 11:54:28 -0400

    fix(license): update lichen.sh to remove local build path information

--- scripts/lichen.sh
+++ scripts/lichen.sh
@@ -2,7 +2,7 @@
 
 go build -o ethereum-mcp.so ./main.go
 go tool lichen --json sbom.tmp ethereum-mcp.so
-cat sbom.tmp | jq 'del(.Modules[].Dir, .Modules[].Licenses[].Path, .Modules[].UsedBy)' > sbom.json
+cat sbom.tmp | jq 'del(.Modules[].Dir, .Modules[].Licenses[].Path, .Modules[].UsedBy, .Binaries[].Path)' > sbom.json
 
 rm sbom.tmp
 rm ethereum-mcp.so
